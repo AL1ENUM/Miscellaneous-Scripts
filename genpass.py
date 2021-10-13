8da682439e0d548798008c887aa8950accc35866
alienum <67610662+AL1ENUM@users.noreply.github.com>
2021-10-13 10:50:51 +0300

    Create genpass.py

---
+++ genpass.py
@@ -0,0 +1,7 @@
+import random
+import string
+
+
+characters = string.ascii_letters + string.digits + string.punctuation
+password = ''.join(random.choice(characters) for i in range(15))
+print(password)
